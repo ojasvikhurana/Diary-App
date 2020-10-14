78e56e2300e987192ecff3b7758343547d99c260
ojasvik <khuranaojasvi@gmail.com>
2020-10-15 02:51:50 +0530

    added navbar and semantic ui

---
+++ README.md
@@ -0,0 +1,4 @@
+# Blog App
+
+- It allows user to add, show, update and delete their blogs.
+- Tech Stack Used - HTML/CSS, Semantic UI, express, MongoDB
---
+++ app.js
@@ -0,0 +1,43 @@
+let express = require("express");
+let app = express();
+let bodyParser = require("body-parser");
+let mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/blogdata",{useNewUrlParser:true,useUnifiedTopology:true});
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+//mongoose model config
+let blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type:Date,default: Date.now}
+});
+
+let Blog = mongoose.model("Blog",blogSchema);
+
+// Blog.create({
+//     title: "testblog",
+//     image: "https://images.unsplash.com/photo-1517607648415-b431854daa86?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=892&q=80",
+//     body: "Love is eternal"
+// });
+
+//RESTful routes
+app.get("/",function(req,res){
+    res.redirect("/blogs");
+})
+
+app.get("/blogs",function(req,res){
+    Blog.find({},function(err,blogs){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("index",{blogs:blogs});
+        }
+    });
+});
+
+
+app.listen(3000);
